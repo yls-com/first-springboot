28c18040b36fe4121936dee244822ef8df00a29f
yulinshi123 <1613302001@qq.com>
2025-09-11 16:49:54 +0800

    1.创建Springboot项目

---
+++ src/main/java/com/ny/NewIot20220217079Application.java
@@ -0,0 +1,13 @@
+package com.ny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewIot20220217079Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewIot20220217079Application.class, args);
+    }
+
+}
